43d4134b71d0a15925893e28ede7dd98250b3682
AdnanRumi <mugterib@gmail.com>
2021-06-13 21:40:19 +0200

    Second commit

--- src/test/java/PeaceStop.java
+++ src/test/java/PeaceStop.java
@@ -3,5 +3,7 @@ public class PeaceStop {
     public static void main(String[] args) {
 
         System.out.println("Blessed are the peacemakers ");
+
+        System.out.println("Sending Peace from local to remote");
     }
 }
